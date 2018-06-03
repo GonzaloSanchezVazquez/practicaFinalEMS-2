f1188658f8fa623340efd6cc6428ca1a86862d27
GonzaloSanchezVazquez <gonzalo.sanchezv@alumnos.upm.es>
2018-06-03 08:15:03 +0200

    Añadida la clase GruposClases

---
+++ AppGestionEMS/Models/GrupoClases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEMS.Models
+{
+    public class GrupoClases
+    {
+        public int Id { get; set; }
+        public String NombreGrupo { get; set; }
+        public int MaxAlumGrupo { get; set; }
+    }
+}
